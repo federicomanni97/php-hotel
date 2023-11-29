5354389d4ded6ecadc09d4ef7b276461d45942fc
federicomanni97 <manni_federico@libero.it>
2023-11-29 14:54:21 +0100

    printer $hotels with foreach in a table

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name= "viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title></title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div id="app">
-    </div>
-</body>
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-<script src="js/script.js"></script>
-</html>
+<?php 
+include __DIR__ . '/partials/header.php';
+?>
+
+<main>
+    <table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Hotel Name</th>
+            <th scope="col">Hotel Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance To Center</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($hotels as $hotelList) { ?>
+        <tr>
+            <?php foreach($hotelList as $element) { ?>
+                <td> <?php echo $element ?> </td>
+            <?php } ?>
+        </tr>
+           <?php } ?>
+    </tbody>
+    </table>
+</main>
+
+<?php
+include __DIR__ .'/partials/footer.php';
+?>
---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        Footer
+    </footer>
+</body>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="js/script.js"></script>
+</html>
